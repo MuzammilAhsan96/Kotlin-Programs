0b01738caa58e1bbe0b5d60b2877a7f13b86342c
muzammilahsan96 <muzammilahsan96@gmail.com>
2021-07-26 15:47:42 +0530

    reverse done

--- app/src/main/java/com/example/kotlin_app/kotlinPractice/StringPrograms.kt
+++ app/src/main/java/com/example/kotlin_app/kotlinPractice/StringPrograms.kt
@@ -18,6 +18,18 @@ fun main() {
     {
         println("Not Anagrams")
     }
+    var count1:IntArray=printDup("rajababajaka")
+    println("Non Repeated Characters")
+    for (i in 0..255)
+    {
+        if (count1[i]==1)
+        {
+            println(Char(i))
+        }
+    }
+    print("Reversed string = ")
+    reverse("abcd")
+    println()
 
 }
 fun printDup(s:String):IntArray
@@ -51,4 +63,23 @@ fun anagrams(s1:String,s2:String):Boolean
         }
     }
     return false
+}
+
+fun printNonrepeated(s:String):IntArray
+{
+    var count1: IntArray = printDup(s)
+    return count1
+}
+
+fun reverse(s:String)
+{
+    if(s.length<2)
+    {
+        print(s)
+    }
+    else
+    {
+        print(s[s.length-1])
+        reverse(s.substring(0,s.length-1))
+    }
 }
